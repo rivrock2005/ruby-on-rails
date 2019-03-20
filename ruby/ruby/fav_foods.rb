441f49ee52e895cde063681fb93d822ad6612bfa
rivrock2005 <44785849+rivrock2005@users.noreply.github.com>
2019-03-20 11:10:54 -0700

    Add files via upload

---
+++ ruby/ruby/fav_foods.rb
@@ -0,0 +1,14 @@
+def fav_foods
+ food_array = []
+ 3.times do
+ puts "Name a favorite food."
+ food_array << gets.chomp
+ p food_array
+ puts "Your favorite foods are #{food_array.join(", ")}."
+ food_array.each do |food|
+   puts "I like #{food} too!"
+
+end
+  end
+    end
+  fav_foods
---
+++ ruby/ruby/program.rb
@@ -0,0 +1,8 @@
+
+def greeting
+  puts "Please enter your name:"
+  name = gets.chomp
+  greet = " your awesome"
+  puts "Hello" + " " + name + greet
+end
+greeting
